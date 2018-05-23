6acfe3b9e1e12f3b83fee7c8144b486f1854d066
MUTUGI <mutahi@localhost.localdomain>
2018-05-22 21:24:54 -0400

    First commit

---
+++ division.py
@@ -0,0 +1,22 @@
+# where a is the divisor
+# where b is the divider
+
+
+def divide(a, b):
+    # initialice the quotient to zero
+    quotient = 0
+    if b == 0:
+        return("cannot divide by zero")
+    elif a == b:
+        return(1, 0)
+    elif b > a:
+        return(0, a)
+
+    else:
+        quotient = (quotient + 1)
+        a = (a-b)
+    return(quotient, a)
+
+
+
+
